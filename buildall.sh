1d69dfc79f96e0c50030654930cef6ad13b7b25b
James Manton <ajd.manton@googlemail.com>
2014-09-10 20:49:50 +0100

    Fix directory-changing logic

--- buildall.sh
+++ buildall.sh
@@ -2,7 +2,9 @@
 library(methods)
 library(rmarkdown)
 
+root_dir <- getwd()
 rmarkdown_files <- dir(pattern=".Rmd", recursive=TRUE)
+rmarkdown_files <- file.path(root_dir, rmarkdown_files)
 built_files <- sapply(rmarkdown_files, function(x) { setwd(dirname(x)); render(basename(x)) })
 
 # built_files will not exist if a file failed to build
