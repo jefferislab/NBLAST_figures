a959d6014131663a91a7884c19708f7e634359e0
Gregory Jefferis <jefferis@gmail.com>
2015-03-27 20:10:31 +0000

    fix build error from buildall.R due to missing search matrices
    * scorematrices were not being found when they were just regular matrices because
      the correct environment was not searched
    * explicitly setting to globalenv seems to fix this

--- buildall.R
+++ buildall.R
@@ -25,7 +25,7 @@ if(!file.exists(file.path(data_dir, 'dpscanon.rds'))) {
 root_dir <- getwd()
 rmarkdown_files <- dir(pattern=".Rmd", recursive=TRUE)
 rmarkdown_files <- file.path(root_dir, rmarkdown_files)
-built_files <- sapply(rmarkdown_files, function(x) { setwd(dirname(x)); render(basename(x)) })
+built_files <- sapply(rmarkdown_files, function(x) { setwd(dirname(x)); render(basename(x), envir=globalenv()) })
 
 # built_files will not exist if a file failed to build
 if(!exists("built_files")) quit(status=1) else quit(status=0)
